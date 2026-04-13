ce742626a13b5c343def7b4e34eec1cf5a9c99db
Temiloluwa <temiomodnbi@gmail.com>
2026-04-13 13:14:28 +0100

    Add function to get computer choice

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ const computerScoreDisplay = document.querySelector("#computer-score");
 const resultDisplay = document.querySelector("#result-display");
 const startBtn = document.querySelector("#start-btn");
 
+function getComputerChoice(){
+    const options = ["Rock", "Paper", "Scisccors"];
+    const randomNum = Math.floor(Math.random() * 3);
+    return options[randomNum];
+}
