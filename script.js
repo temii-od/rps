be31057d0b55d40a7443d07d662b224966ccbaa3
Temiloluwa <temiomodnbi@gmail.com>
2026-04-13 13:10:59 +0100

    Using DOM to manipulate the files

---
+++ script.js
@@ -0,0 +1,8 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const humanScoreDisplay = document.querySelector("#human-score");
+const computerScoreDisplay = document.querySelector("#computer-score");
+const resultDisplay = document.querySelector("#result-display");
+const startBtn = document.querySelector("#start-btn");
+
